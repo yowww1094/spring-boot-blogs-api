aab34bc29e1c453b1194268890b32497e2ed6bde
yowww1094 <younesaitsiabou@gmail.com>
2024-03-06 10:58:33 +0100

    files initialization

---
+++ src/main/java/com/demo/blogsapi/BlogsapiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.blogsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogsapiApplication.class, args);
+	}
+
+}
